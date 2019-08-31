bcc577f711feecc6fef8d083064ec2ddb1c6be9d
Maxim Zubkov <Zubkov.MD@phystech.edu>
2019-08-31 19:32:51 +0300

    Updates

---
+++ precache-manifest.37d19f0eb1e1dd7e1de95c8b2a2ce53b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "100e0d18524141cfefbd",
+    "url": "./static/js/main.354954bd.chunk.js"
+  },
+  {
+    "revision": "7c061407ef9f88083494",
+    "url": "./static/js/2.91ef7750.chunk.js"
+  },
+  {
+    "revision": "100e0d18524141cfefbd",
+    "url": "./static/css/main.2c5eaeb9.chunk.css"
+  },
+  {
+    "revision": "7c061407ef9f88083494",
+    "url": "./static/css/2.3211dee2.chunk.css"
+  },
+  {
+    "revision": "8ebd03b4b9744d344343592bd97c5e0d",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b10ef8617c5fc6041d27d6154d8b52a9.js"
+  "./precache-manifest.37d19f0eb1e1dd7e1de95c8b2a2ce53b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
